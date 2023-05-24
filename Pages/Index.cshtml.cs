e730029aad0e6cc31cf487238a29e780c3602a53
chadbentz <chadbentz@SURFACE-LAPTOP5>
2023-05-24 11:51:10 -0700

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace netcore_mvc_razor_xss.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string sanitizedData { get; set; }
+        public string unsanitizedData { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            var data = Request.Query["data"];
+
+            sanitizedData = System.Net.WebUtility.HtmlEncode(data);
+            unsanitizedData = data;
+        }
+    }
+}
